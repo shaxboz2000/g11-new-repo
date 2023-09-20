a9fb7daa0a6bdb21da3ca017bbb2e7c607db99ae
shaxboz2000 <saxbozkalmuratov@gmail.com>
2023-09-20 12:25:25 +0500

    yangi ss

---
+++ projs/x-nolck/scirpt.js
@@ -0,0 +1,15 @@
+
+
+
+
+const number = document.querySelector("#number");
+const second = document.querySelector("#second");
+const btn = document.querySelector(".btn");
+const num = document.querySelector(".num");
+
+btn.addEventListener("click", () => {
+   setTimeout(() => {
+  num.innerText = eval(`${num.innerText}+${number.value}`);
+  }, `${second.value}000`);
+ 
+})
